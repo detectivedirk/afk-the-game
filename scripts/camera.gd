c27884a1acd22a109e6610e9ba7cd5da8535367b
dirk <darikkw@gmail.com>
2025-05-09 20:56:20 +0300

    added character and camera controls

---
+++ scripts/camera.gd
@@ -0,0 +1,21 @@
+extends SpringArm3D
+
+var sensibility: float = 0.005
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("wheel_down"):
+		spring_length += 0.5
+	if Input.is_action_just_released("wheel_up"):
+		spring_length -= 0.5
+	spring_length = clamp(spring_length, 1, 10)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_move_camera(event)
+	
+func _move_camera(event: InputEventMouseMotion):
+	rotation.y -= event.relative.x * sensibility
+	rotation.x -= event.relative.y * sensibility
---
+++ scripts/player.gd
@@ -0,0 +1,77 @@
+extends CharacterBody3D
+
+const COYOTE_TIME = 0.15
+const BUFFER_TIME = 0.15
+
+var speed = 10.0
+
+var speed_boost = 1
+
+const JUMP_HEIGHT = 1
+const JUMP_TIME_TO_PEAK = 0.35
+const JUMP_TIME_TO_DESCENT = 0.25
+
+const TERMINAL_VELOCITY = 300
+
+var coyote_timer: float = 0
+var buffer_timer: float = 0
+
+@onready var jump_velocity : float = ((2.0 * JUMP_HEIGHT) / JUMP_TIME_TO_PEAK) * 1.0
+@onready var jump_gravity : float = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_PEAK * JUMP_TIME_TO_PEAK)) * 1.0
+@onready var fall_gravity : float = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_DESCENT * JUMP_TIME_TO_DESCENT)) * 1.0
+
+@onready var camera = $camera_stand
+@onready var model = $model
+
+var camera_distance: float = 1
+	
+func get_jump_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	_move_player(delta)
+	
+func _move_player(delta: float):
+	var movement_vector: Vector2
+	if Input.is_action_pressed("forward"):
+		movement_vector = Vector2.UP
+	elif Input.is_action_pressed("left"):
+		movement_vector = Vector2.LEFT
+	elif Input.is_action_pressed("back"):
+		movement_vector = Vector2.DOWN
+	elif Input.is_action_pressed("right"):
+		movement_vector = Vector2.RIGHT
+		
+	movement_vector = movement_vector.rotated(-camera.rotation.y) * speed	
+	
+	velocity.x = movement_vector.x
+	velocity.z = movement_vector.y
+	
+	if movement_vector != Vector2.ZERO:
+		model.rotation.y = camera.rotation.y + PI
+	
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+	else:
+		coyote_timer -= delta
+		velocity.y += get_jump_gravity() * delta
+
+	buffer_timer -= delta
+	if Input.is_action_just_pressed("jump"):
+		buffer_timer = BUFFER_TIME
+		
+	if buffer_timer > 0 and coyote_timer > 0:
+		buffer_timer = 0
+		coyote_timer = 0
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_released("jump") && velocity.y > 0:
+		velocity.y = 0
+	
+	move_and_slide()
